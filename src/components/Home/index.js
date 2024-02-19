33ebdaedeef05405817b09a644c7b00731977b78
aravinthmra <aravinthmspv@gmail.com>
2024-02-19 08:37:21 +0000

    navbar added

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div className="bg-container-home">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import logo from "../../assets/logo.jpg";
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <nav className="px-5 py-3 flex justify-between font-sans font-semibold">
+      <div className="flex items-center">
+        <img className="w-16 rounded-lg" src={logo} alt="ahead-logo" />
+      </div>
+      <ul className="w-100 flex justify-between items-center">
+        <li className="mx-3">Emotions</li>
+        <li className="mx-3">Manifesto</li>
+        <li className="mx-3">Self-awareness test</li>
+        <li className="mx-3">Work With Us</li>
+      </ul>
+      <div className="flex items-center">
+        <button className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full">
+          Download app
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
